f22c4ff5d95052393acfccd94eaaa9885070b5bb
galok8749 <galok8749@gmailcom>
2023-02-03 20:47:25 +0530

    employee added to database

--- src/main/java/com/alok/employeemanagement/service/EmpService.java
+++ src/main/java/com/alok/employeemanagement/service/EmpService.java
@@ -3,7 +3,13 @@ package com.alok.employeemanagement.service;
 import com.alok.employeemanagement.entity.Employee;
 import com.alok.employeemanagement.repository.EmpRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpService {
@@ -11,5 +17,26 @@ public class EmpService {
     private EmpRepository empRepository;
     public void addEmp(Employee e) {
         empRepository.save(e);
+    }
+
+    public List<Employee> getAllEmp() {
+        return empRepository.findAll();
+    }
+
+    public Employee getEMpById(int id) {
+        Optional<Employee> e = empRepository.findById(id);
+        if (e.isPresent()) {
+            return e.get();
+        }
+        return null;
+    }
+
+    public void deleteEMp(int id) {
+        empRepository.deleteById(id);
+    }
+
+    public Page<Employee> getEMpByPaginate(int currentPage, int size) {
+        Pageable p = PageRequest.of(currentPage, size);
+        return empRepository.findAll(p);
     }
 }
